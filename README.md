8f2bafbbb87e9b8a0543e9dd261d5a6e0fe24864
Henry Zhao <henryzhao@me.com>
2017-09-16 22:23:15 +1000

    Initial public pages commit
    'username' removed from Users table, use 'email'

--- README.md
+++ README.md
@@ -8,4 +8,4 @@
 4. Change MySQL port to match config/config.json
 5. Create user 'budgetApp'@'localhost' password 'budget123./' in MySQL
 6. Create database 'budgetapp' and grant privileges to user
-7. `node bin/www` to start
+7. `npm start`
--- budgetApp/models/user.js
+++ budgetApp/models/user.js
@@ -2,10 +2,6 @@
 
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
-    username: {
-      type: DataTypes.STRING,
-      unique: true
-    },
     email: {
       type: DataTypes.STRING,
       unique: true
