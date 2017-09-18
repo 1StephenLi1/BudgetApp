56e0dd1b3802f2ff0a961b8fcd10678d0c6c7e21
Henry Zhao <henryzhao@me.com>
2017-09-18 23:14:20 +1000

    Added routes/signup.js
    Sorry

---
+++ budgetApp/routes/signup.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+var models  = require('../models');
+var auth = require('../auth.js');
+
+/* POST signup */
+router.post('/', function(req, res, next) {
+	// console.log("Received " + req.body.email);
+	var user = {
+		"email": req.body.email,
+		"password": req.body.password,
+		"firstName": req.body.firstName,
+		"lastName": req.body.lastName,
+		"salt": auth.genSalt(128)
+	}
+
+	user.password = auth.sha512(user.password, user.salt);
+
+	models.User.create(user).then(function(u) {
+		res.redirect('/');
+	}).catch(function(error) {
+		req.flash('signup', 'An account already exists with email ' + user.email);
+		res.redirect('/login');
+	});
+});
+
+module.exports = router;
