97eab1a702ded209cd7a0c4fa24d82bcfb5943bc
1StephenLi1 <fanmo97@gmail.com>
2017-09-25 01:09:58 +1000

    merged uploadCsv.js with expenses.js

--- budgetApp/routes/expenses.js
+++ budgetApp/routes/expenses.js
@@ -2,7 +2,13 @@ var express = require('express');
 var router = express.Router();
 var models = require('../models');
 var moment = require('moment-timezone');
+var csv = require('fast-csv');
+var fs = require('fs');
 
+var shortDescription;
+var longDescription;
+var amount;
+var dateTime;
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Show sonthing here...' });
@@ -19,11 +25,13 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/addExpense', function(req, res) {
+
     res.render('addExpense', {
         title: 'Add Expense',
         user: req.session.user
     })
 })
+
 
 router.post('/addExpense', function(req, res) {
     if (req.session.user == null || req.session.user.id == null) {
@@ -65,6 +73,69 @@ router.post('/addExpense', function(req, res) {
         })
     }
 })
+
+
+router.get('/uploadCsv', function(req, res) {
+    res.render('uploadCsv', {
+        title: 'Upload CSV',
+        user: req.session.user
+    })
+})
+
+router.post('/uploadCsv', function(req, res) {
+   if (!req.files)
+    return res.status(400).send('No files were uploaded.');
+ 
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  let csvFile = req.files.mycsv;
+  let fliePath = __dirname+'';
+  let newFilePath = '';
+  let arr = fliePath.split('\\');
+  
+  for (let i = 0; i < arr.length -1; i++) {
+    newFilePath += arr[i]+'/';
+  }
+  // Use the mv() method to place the file somewhere on your server
+  csvFile.mv(newFilePath + '/csv/' + csvFile.name, function(err) {
+    if (err) {
+        return res.status(500).render('uploadCsv', { title: err });
+    } else {
+        res.send('File uploaded!');
+        var stream = fs.createReadStream(newFilePath + '/csv/' + csvFile.name);
+
+        csv
+        .fromStream(stream, {headers : true})
+        .on("data", function(data){
+            amount = data['Amount'],
+            shortDescription = data['Short Description'],
+            longDescription = data['Long Description'],
+            dateTime = data['DateTime'],
+            models.Cashflow.create({
+                shortDescription: shortDescription,
+                longDescription: longDescription,
+                amount: amount,
+                //dateTime: dateTime,
+                //UserId: req.session.user.id
+            }).then(function(expense) {
+                if (expense == null) {
+                    res.status(400).json({
+                        errorMsg: "An error occured, try again later"
+                    })
+                } else {
+                    res.status(200).json({
+                        msg: "Expense was created"
+                    })
+                }
+            })
+        }).on("end", function(){
+        console.log("done");
+    });
+    }
+              
+  });
+
+}); 
+
 
 
 module.exports = router;
