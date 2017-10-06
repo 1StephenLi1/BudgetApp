57e70b1aff038cac1ded68ab907d5943a6cbde43
Henry Zhao <henryzhao@me.com>
2017-10-06 22:45:31 +1100

    Dashboard bar chart and pie chart improvements

--- budgetApp/public/javascripts/dashboard.js
+++ budgetApp/public/javascripts/dashboard.js
@@ -1,26 +1,95 @@
+var date = moment().subtract(1, 'months').format('YYYY-MM-DD');;
+var timeBar;
+var categoriesPie;
+
 $(document).ready(function() {
-    $.getJSON('/dashboard/categories', function (categories) {
-        updateCategoriesPie(categories);
+    $("input:radio[name=filter]").change(function() {
+        var selectedFilter = $("input:radio[name=filter]:checked");
+        if (selectedFilter.attr('id') == "month") {
+            date = moment().subtract(1, 'months').format('YYYY-MM-DD');
+        } else if (selectedFilter.attr('id') == "quarter") {
+            date = moment().subtract(3, 'months').format('YYYY-MM-DD');
+        } else {
+            date = moment().subtract(1, 'years').format('YYYY-MM-DD');
+        }
+        updatePage();
     });
+    updatePage();
 });
 
+function updatePage() {
+    $.getJSON('/dashboard/categories?date=' + date, function (categories) {
+        updateCategoriesPie(categories);
+    });
+    var filter = $("input:radio[name=filter]:checked").attr('id');
+    $.getJSON('/dashboard/time?date=' + date + '&filter=' + filter, function (data) {
+        updateTimeBar(data);
+    });
+}
+
+function updateTimeBar(data) {
+    var timeBarCtx = document.getElementById('time-bar').getContext('2d');
+    if (timeBar != undefined) timeBar.destroy();
+    timeBar = new Chart(timeBarCtx,{
+        type: 'bar',
+        data: {
+            datasets: [{
+                data: data.totals,
+                backgroundColor: [
+                    "rgb(255, 99, 132)",
+                    "rgb(54, 162, 235)",
+                    "rgb(255, 205, 86)"
+                ]
+            }],
+            labels: data.dates
+        },
+        options: {
+            title: {
+                display: true,
+                fontSize: 24,
+                text: "Spending Over Time"
+            },
+            scales: {
+                xAxes: [{
+                    gridLines: false
+                }],
+                yAxes: [{
+                    gridLines: false,
+                    ticks: {
+                        min: 0,
+                        maxTicksLimit: 4
+                    }
+                }]
+            },
+            legend: {
+                display: false
+            }
+        }
+    });
+}
+
 function updateCategoriesPie(categories) {
-    data = {
-        datasets: [{
-            data: categories.totals,
-            backgroundColor: [
-                "rgb(255, 99, 132)",
-                "rgb(54, 162, 235)",
-                "rgb(255, 205, 86)"
-            ]
-        }],
-        labels: categories.names
-    }
-
-    var categoriesPieContext = document.getElementById('categories-pie').getContext('2d');
-
-    var categoriesPie = new Chart(categoriesPieContext,{
+    var categoriesPieCtx = document.getElementById('categories-pie').getContext('2d');
+    if (categoriesPie != undefined) categoriesPie.destroy();
+    categoriesPie = new Chart(categoriesPieCtx,{
         type: 'pie',
-        data: data
+        data: {
+            datasets: [{
+                data: categories.totals,
+                backgroundColor: [
+                    "rgb(255, 99, 132)",
+                    "rgb(54, 162, 235)",
+                    "rgb(255, 205, 86)"
+                ]
+            }],
+            labels: categories.names
+        },
+        options: {
+            title: {
+                display: true,
+                fontSize: 24,
+                text: "Spending by Category"
+            }
+        }
     });
 }
