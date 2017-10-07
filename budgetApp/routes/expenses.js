254b9e405d89e2bc50694449d1809b5d68fc6b55
1StephenLi1 <fanmo97@gmail.com>
2017-10-07 20:10:57 +1100

    upload now handles multiple records

--- budgetApp/routes/expenses.js
+++ budgetApp/routes/expenses.js
@@ -8,12 +8,7 @@ var mv = require('mv');
 var http = require('http');
 var json2csv = require('json2csv');
 
-var shortDescription;
-var longDescription;
-var category;
-var date;
-var amount;
-var dateTime;
+
 
 /* GET home page. */
 
@@ -79,15 +74,23 @@ router.post('/uploadCsv', function(req, res) {
 
   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
   let csvFile = req.files.mycsv;
-  let fliePath = __dirname+'';
+  let filePath = __dirname+'';
   let newFilePath = '';
-  let arr = fliePath.split('\\');
-
+  let arr = filePath.split('/');
+  console.log("filePath: " + filePath);
+  console.log("array after split: " + arr);
   for (let i = 0; i < arr.length -1; i++) {
     newFilePath += arr[i]+'/';
   }
+
+  console.log("newFilePath: " + newFilePath);
   // Use the mv() method to place the file somewhere on your server
   csvFile.mv(newFilePath + '/bin/' + csvFile.name, function(err) {
+    var amount = 0;
+    var shortDescription = null;
+    var longDescription = null;
+    var date = null;
+    var category = null;
     if (err) {
         return res.status(500).render('uploadCsv', { title: err, user: req.session.user.id});
     } else {
@@ -97,24 +100,22 @@ router.post('/uploadCsv', function(req, res) {
         user: req.session.user
     })
         csv
-        .fromStream(stream, {headers : true})
-        .on("data", function(data){
-            // console.log('whole data: ' + data);
+        .fromStream(stream, {headers: true})
+        .transform(function(data, next){
             amount = data['Amount'],
             shortDescription = data['Short Description'],
             longDescription = data['Long Description'],
             date = data['Expense Date'],
             category = data['Category'],
-            // console.log('Amount: ' + amount);
             models.Category.findOrCreate({
-                where: {
-                    "UserId": req.session.user.id,
-                    "type": "expense",
-                    "name": category
-                },
-                default: {
-                    "isArchived": 0
-                }
+            where: {
+                "UserId": req.session.user.id,
+                "type": "expense",
+                "name": category
+            },
+            default: {
+                "isArchived": 0
+            }
             }).then(function([category, isNewlyCreated]) {
                 models.Cashflow.create({
                 dateTime: moment(date,'DD/MM/YYYY').tz("Australia/Sydney"),
@@ -130,22 +131,28 @@ router.post('/uploadCsv', function(req, res) {
                         errorMsg: "An error occured, try again later"
                     })
                 } else {
-                    res.status(200).json({
-                        msg: "Expense added successfully"
-                    })
+                    // res.status(200).json({
+                    //     msg: "Expense added successfully"
+                    // })
+                   
                 }
-            })
-        }).catch(function (err) {
-            console.error(err);
-            res.status(err.status || 500);
-            res.render('error', {
-                user: req.session.user
-            });
-        });
 
-        })//for on
-        .on("end", function(){
-            // console.log("done");
+            })
+             next();
+            }).catch(function (err) {
+                console.error(err);
+                res.status(err.status || 500);
+                    res.render('error', {
+                    user: req.session.user
+                });
+            });
+
+        })
+            
+        .on("data", function(data){
+
+        }).on("end", function(){
+           
         });
     }
 });
@@ -162,28 +169,33 @@ router.get('/export', function(req, res) {
                     errorMsg: "An error occured, try again later"
                 })
             } else {
-                var fields = ['Category', 'Amount', 'Short Description', 'Long Description', 'Expense Date'];
+                var fields = ['Expense Date', 'Amount', 'Short Description', 'Long Description'];
                 // var arrayList;
-                // for (var i in cashflow) {
-                    var myCsv = [
-                      {
-                        "Category": "Expense",
-                        "Amount": cashflow[0].dataValues.amount,
-                        "Short Description": cashflow[0].dataValues.shortDescription,
-                        "Long Description": cashflow[0].dataValues.longDescription,
-                        "Expense Date": cashflow[0].dataValues.dateTime
-                      }
-                    ];
-                    // var obj = JSON.parse(myCsv);
+                    var data=[];
+                    
+                    for (var i = 0; i < cashflow.length; i++) {
+                        
+                        data[data.length] = {
+                            "Amount": cashflow[i].dataValues.amount,
+                            "Short Description": cashflow[i].dataValues.shortDescription,
+                            "Long Description": cashflow[i].dataValues.longDescription,
+                            "Expense Date": cashflow[i].dataValues.dateTime
+                            }
+                    }
+                    //var obj = JSON.parse(myCsv);
                     // arrayList.push(obj);
                 // }
                 // var finalCsv = JSON.stringify(arrayList);
-                var csv = json2csv({ data: myCsv, fields: fields });
-                 
-                fs.writeFile('expense.csv', csv, function(err) {
+                //var csv = json2csv({ data: data, fields: fields });
+                 json2csv({ data: data, fields: fields }, function(err, csv) {
+                  if (err) console.log(err);
+                  console.log(csv);
+                  fs.writeFile('expense.csv', csv, function(err) {
                   if (err) throw err;
-                  res.download('file.csv');
+                  res.download('expense.csv');
                 });
+                });
+                
                 }
         })
 })
